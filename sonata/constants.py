4aaa1573933bbcf86d6505c056af7fde8ce222a9
hwk06023 <hwk06023@gmail.com>
2025-04-16 17:58:17 +0900

    feat: refactor constants to use enum types

--- sonata/constants.py
+++ sonata/constants.py
@@ -1,20 +1,61 @@
 """
 Constants for SONATA project.
 """
-# Thresholds
+from enum import Enum, auto
+
+
+class LanguageCode(str, Enum):
+    """ISO 639-1 language codes for supported languages."""
+
+    ENGLISH = "en"
+    KOREAN = "ko"
+    CHINESE = "zh"
+    JAPANESE = "ja"
+    FRENCH = "fr"
+    GERMAN = "de"
+    SPANISH = "es"
+    ITALIAN = "it"
+    PORTUGUESE = "pt"
+    RUSSIAN = "ru"
+
+
+class FormatType(str, Enum):
+    """Transcript format types."""
+
+    CONCISE = "concise"  # Simple text with emotive tags
+    DEFAULT = "default"  # Text with timestamps
+    EXTENDED = "extended"  # With confidence scores
+
+
+class EmotiveEventType(str, Enum):
+    """Types of emotive events that can be detected."""
+
+    LAUGH = "laugh"
+    SIGH = "sigh"
+    INHALE = "inhale"
+    GROAN = "groan"
+    COUGH = "cough"
+    SNEEZE = "sneeze"
+    SNIFFLE = "sniffle"
+    SURPRISE = "surprise"
+    YAWN = "yawn"
+    WHIMPER = "whimper"
+
+
+# Threshold values
 EMOTIVE_THRESHOLD = 0.3  # Default threshold for detecting emotive events
 
 # Default settings
-DEFAULT_LANGUAGE = "en"
+DEFAULT_LANGUAGE = LanguageCode.ENGLISH.value
 DEFAULT_MODEL = "large-v3"
 DEFAULT_DEVICE = "cpu"
 DEFAULT_COMPUTE_TYPE = "float32"
 
-# Format types
-FORMAT_CONCISE = "concise"
-FORMAT_DEFAULT = "default"
-FORMAT_EXTENDED = "extended"
-
 # Split settings
 DEFAULT_SPLIT_LENGTH = 30  # Length of split segments in seconds
 DEFAULT_SPLIT_OVERLAP = 5  # Overlap between split segments in seconds
+
+# Format types for backwards compatibility
+FORMAT_CONCISE = FormatType.CONCISE.value
+FORMAT_DEFAULT = FormatType.DEFAULT.value
+FORMAT_EXTENDED = FormatType.EXTENDED.value
